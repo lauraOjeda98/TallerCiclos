51c27ffdaa658d8b21174ca9e88dd06d9452ccef
lauraOjeda98 <70666704+lauraOjeda98@users.noreply.github.com>
2021-09-20 14:02:47 -0500

    Séptimo punto

--- TallerCiclos.py
+++ TallerCiclos.py
@@ -178,3 +178,27 @@ def peso():
         else:
             print(f"Tiene un promedio de: {prom_peso} kg")
             print(f"La persona {i+1} BAJÓ de peso con {peso-prom_peso} kg")
+
+# 7. En un supermercado una ama de casa pone en su carrito los artículos que
+# va tomando de los estantes. La señora quiere asegurarse de que el cajero
+# le cobre bien lo que ella ha comprado, por lo que cada vez que toma un
+# artóculo anota su precio junto con la cantidad de artículos iguales que ha
+# tomado y determina cuanto dinero gastará en ese artículo; a esto le suma lo
+# que irá gastando en los demás artículos, hasta que decide que ya tomó
+# todo lo que necesitaba. Ayúdele a esta señora a obtener el total de su
+# compra.
+
+
+def calcularCompra():
+    compra = True
+    total = 0
+
+    while compra:
+        cant = int(input("Ingrese la cantidad del producto: "))
+        precio = float(input("Ingrese el precio por producto: "))
+        total += cant * precio
+
+        resp = input("¿Desea finalizar su compra? (si o no): ")
+        if resp == "si":
+            compra = False
+    print(f"El total a pagar es de: ${total:,}")
